3f6e1bbd128e9c84f14b3c04158aad5dc1ec11ee
System Administrator <root@ddd.local>
2021-09-15 12:01:22 +0530

    new test

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:practical/ui/country_screen.dart';
 
-
 Dio dio = Dio();
-
-
 
 void main() {
   runApp(const MyApp());
@@ -14,14 +11,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: const CountryScreen(),
--- lib/ui/country_list_item.dart
+++ lib/ui/country_list_item.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:practical/model/country_model.dart';
+import 'package:practical/ui/country_detail_screen.dart';
 
 class CountryListTile extends StatelessWidget {
   final CountryModel country;
@@ -8,66 +9,67 @@ class CountryListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-        child: Container(
-            padding: const EdgeInsets.all(10),
-            child: Row(
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.25,
-                  height: MediaQuery.of(context).size.width * 0.25,
-                  child: SvgPicture.network(
-                    country.flag,
-                    // semanticsLabel: 'Flag',
-                    fit: BoxFit.contain,
-                    allowDrawingOutsideViewBox: false,
-                    excludeFromSemantics: false,
+    return InkWell(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (_) => CountryDetailScreen(country)));
+      },
+      child: Card(
+          child: Container(
+              padding: const EdgeInsets.all(10),
+              child: Row(
+                children: [
+                  SizedBox(
                     width: MediaQuery.of(context).size.width * 0.25,
                     height: MediaQuery.of(context).size.width * 0.25,
-                    placeholderBuilder: (BuildContext context) => Container(
-                        padding: const EdgeInsets.all(30.0),
-                        child: const CircularProgressIndicator()),
+                    child: SvgPicture.network(
+                      country.flag,
+                      // semanticsLabel: 'Flag',
+                      fit: BoxFit.contain,
+                      allowDrawingOutsideViewBox: false,
+                      excludeFromSemantics: false,
+                      width: MediaQuery.of(context).size.width * 0.25,
+                      height: MediaQuery.of(context).size.width * 0.25,
+                      placeholderBuilder: (BuildContext context) =>
+                          Container(padding: const EdgeInsets.all(30.0), child: const CircularProgressIndicator()),
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(country.name,
-                          maxLines: 2,
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(country.name,
+                            maxLines: 2,
+                            style: const TextStyle(
+                              color: Colors.black,
+                              fontSize: 18,
+                            )),
+                        Text(
+                          country.capital,
                           style: const TextStyle(
                             color: Colors.black,
-                            fontSize: 18,
-                          )),
-                      Text(
-                        country.capital,
-                        style: const TextStyle(
-                          color: Colors.black,
-                          fontSize: 16,
+                            fontSize: 16,
+                          ),
                         ),
-                      ),
-                      Text(
-                        "population : " + country.population.toString(),
-                        style: const TextStyle(
-                          color: Colors.black,
-                          fontSize: 14,
-                        ),
-                      ),
-                      Text(
-                          "currency : " +
-                              country.currencies.first.name +
-                              "(${country.currencies.first.code})",
+                        Text(
+                          "population : " + country.population.toString(),
                           style: const TextStyle(
                             color: Colors.black,
                             fontSize: 14,
-                          )),
-                    ],
+                          ),
+                        ),
+                        Text("currency : " + country.currencies.first.name + "(${country.currencies.first.code})",
+                            style: const TextStyle(
+                              color: Colors.black,
+                              fontSize: 14,
+                            )),
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            )));
+                ],
+              ))),
+    );
   }
 }
